e2f6defda6f3691d63082397fceb5dd56c124e6e
Ryan Gang <ryan-gg@outlook.com>
2023-12-08 20:50:20 +0530

    Stage 1. Initialize the .git directory.

--- app/main.py
+++ app/main.py
@@ -1,23 +1,28 @@
-import sys
 import os
+import sys
+
+
+def create_repo():
+    """
+    Create an empty .git directory, and the bare minimum subdirectories required for git to function.
+    """
+    os.mkdir(".git")
+    os.mkdir(".git/objects")
+    os.mkdir(".git/refs")
+    with open(".git/HEAD", "w") as f:
+        f.write("ref: refs/heads/master\n")
+    print(f"Initialized empty Git repository.")
 
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this block to pass the first stage
-    #
-    # command = sys.argv[1]
-    # if command == "init":
-    #     os.mkdir(".git")
-    #     os.mkdir(".git/objects")
-    #     os.mkdir(".git/refs")
-    #     with open(".git/HEAD", "w") as f:
-    #         f.write("ref: refs/heads/master\n")
-    #     print("Initialized git directory")
-    # else:
-    #     raise RuntimeError(f"Unknown command #{command}")
+    # print("Logs from your program will appear here!")
+    command = sys.argv[1]
+    match command:
+        case "init":
+            create_repo()
+        case _:
+            raise RuntimeError(f"Unknown command #{command}")
 
 
 if __name__ == "__main__":
