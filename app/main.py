cb852f7762f93db091746e895143834a816ccd33
Ryan Gang <ryan-gg@outlook.com>
2023-12-08 21:40:11 +0530

    Stage 2. Read a blob object.

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 import os
 import sys
+import zlib
 
 
 def create_repo():
     """
-    Create an empty .git directory, and the bare minimum subdirectories required for git to function.
+    Creates an empty .git directory, and the bare minimum subdirectories
+    required for git to function.
     """
     os.mkdir(".git")
     os.mkdir(".git/objects")
@@ -14,6 +16,29 @@ def create_repo():
     print(f"Initialized empty Git repository.")
 
 
+def read_blob(blob_sha: str) -> str:
+    """
+    Retrieves the contents of a git blob object given its SHA-1 hash.
+    Decompresses it, decodes it and returns an utf-8 encoded string.
+    """
+    dir, file = blob_sha[:2], blob_sha[2:]
+    full_path = f".git/objects/{dir}/{file}"
+    with open(full_path, "rb") as blob:
+        data = zlib.decompress(blob.read())
+        text = data.decode(encoding="utf-8")
+        return text
+
+
+def cat_file(option: str, sha: str):
+    """
+    Examines the contents of any given git object, based on its SHA-1 hash, and
+    the options passed through CLI. 
+    """
+    if option == "-p":
+        meta_info, content = read_blob(sha).split("\x00")
+        print(content, end="")
+
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     # print("Logs from your program will appear here!")
@@ -21,6 +46,9 @@ def main():
     match command:
         case "init":
             create_repo()
+        case "cat-file":
+            assert len(sys.argv) == 4, "fatal: wrong number of arguments, should be 4"
+            cat_file(option=sys.argv[2], sha=sys.argv[3])
         case _:
             raise RuntimeError(f"Unknown command #{command}")
 
